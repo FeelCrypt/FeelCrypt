b173b3e1351078a7b8752bbf4a8fbccde16f1041
FrancoisMERCADAL <mercadal.franois7@gmail.com>
2020-01-24 14:28:45 +0100

    API end point add

--- API/PII/predictions/urls.py
+++ API/PII/predictions/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('accueil', views.accueil),
-    path('get_prediction', views.get_prediction)
+    path('get_prediction', views.get_prediction),
+    path('get_prediction_noword', views.get_prediction_noword)
 ]
--- API/PII/predictions/views.py
+++ API/PII/predictions/views.py
@@ -1,6 +1,12 @@
 from datetime import datetime
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+import get_chart_price_cryptocurrencies_csv
+import dataset
+#import keras_bitcoin
+import moment
+import pickle
+import pandas as pd
 
 def home(request):
     """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
@@ -13,8 +19,17 @@ def accueil(request):
     return render(request, "predictions/accueil.html")
 
 def get_prediction(request):
-    dico = {"brand": "Ford", "model": "Mustang", "year": 1964}
-    return JsonResponse(dico, status = 200)
+    #update reddit data
+    df = dataset.read_today_data(threads=["bitcoin", "btc"])
+    text_to_predict = dataset.encode_data(df["text"])
+    xg_class = pickle.load(open("xgboost_bitcoin.dat", "rb"))
+    preds = xg_class.predict(text_to_predict)
+    pred = collections.Counter(preds).most_common()[0][0]
+    #dico = {"brand": "Ford", "model": "Mustang", "year": 1964}
+    return JsonResponse({"pred" : pred}, status = 200)
+
+def get_prediction_noword(request):
+    
 
 
 # Create your views here.
